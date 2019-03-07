1d2eb36825f89426e12d6e42bff08616f3426c88
zloiseleux <47027478+zloiseleux@users.noreply.github.com>
2019-03-07 16:47:01 +0100

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,15 +1,3 @@
 function montre(id){
 		$(id).slideToggle(4000);
 }
-
-function montre2(id){
-		$(id).slideToggle(4000);
-}
-
-function montre3(id){
-		$(id).slideToggle(4000);
-}
-
-function montre4(id){
-		$(id).slideToggle(4000);
-}
